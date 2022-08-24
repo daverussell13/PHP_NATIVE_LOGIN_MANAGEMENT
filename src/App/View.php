3501b74a952cfe6ee038b010824a160e300fbd6b
Dave Russell <daverussell1309@gmail.com>
2022-08-24 23:52:09 +0700

    Add / View

--- src/App/View.php
+++ src/App/View.php
@@ -6,6 +6,8 @@ class View
 {
   public static function render(string $view, mixed $model): void
   {
-    require __DIR__ . "/../Views" . $view . ".php";
+    require __DIR__ . "/../Views/header.php";
+    require __DIR__ . "/../Views/" . $view . ".php";
+    require __DIR__ . "/../Views/footer.php";
   }
 }
