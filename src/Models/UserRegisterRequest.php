f8730b579e607273e0d2ddc114e17861ab055c1d
Dave Russell <daverussell1309@gmail.com>
2022-08-26 03:07:07 +0700

    Add UserRegisterResponse and UserRegisterRequest Model

---
+++ src/Models/UserRegisterRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Daver\MVC\Models;
+
+class UserRegisterRequest
+{
+  public ?string $id = null;
+  public ?string $name = null;
+  public ?string $password = null;
+}
---
+++ src/Models/UserRegisterResponse.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Daver\MVC\Models;
+
+use Daver\MVC\Domain\User;
+
+class UserRegisterResponse
+{
+  public User $user;
+}
