9a5585da8842f9a17d5cf926e65fa07d86fc8c6f
Dave Russell <daverussell1309@gmail.com>
2022-08-25 19:44:12 +0700

    Add UserRepository save and findbyid unit test

---
+++ tests/Repository/UserRepositoryTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Daver\MVC\Repository;
+
+use PHPUnit\Framework\TestCase;
+use Daver\MVC\Config\Database;
+use Daver\MVC\Domain\User;
+
+class UserRepositoryTest extends TestCase
+{
+  private UserRepository $repository;
+
+  public function setUp(): void
+  {
+    $this->repository = new UserRepository(Database::getConnection());
+    $this->repository->deleteAll();
+  }
+
+  public function testSaveSuccess(): void
+  {
+    $user = new User();
+
+    $user->setId("1")
+         ->setName("test")
+         ->setPassword("test");
+
+    $result = $this->repository->save($user);
+    self::assertEquals($user, $result);
+
+    $getUser = $this->repository->findById($user->getId());
+    self::assertEquals($user, $getUser);
+  }
+
+  public function testFindByIdNotFound(): void
+  {
+    $getUser = $this->repository->findById("1");
+    self::assertNull($getUser);
+  }
+}
